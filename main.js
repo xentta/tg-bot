28e570d8e30f70063ace093b2e1bdefeaa28fc0f
markov <markov@axsoft.ru>
2024-12-25 10:13:12 +0300

    add method on

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { Telegraf, Markup } from 'telegraf'
+import { message } from 'telegraf/filters'
 
 const token = '7946503633:AAHvng-wwXe4jZ8smNs9NbPKaF-U0FoBFpM'
 const webAppUrl = 'https://dev.gz.mos.ru'
@@ -6,5 +7,9 @@ const bot = new Telegraf(token)
 bot.command('start', (ctx) => {
     ctx.reply('Привет, это тестовое сообщение',
     Markup.keyboard([Markup.button.webApp('Открыть приложение Город  Заданий', webAppUrl)]))
+})
+bot.on(message('web_app_data'), async ctx => {
+    const data = ctx.webAppData.data.json()
+    ctx.reply(`Поздравляю: ${data}`)
 })
 bot.launch()
