5d8b84ed885d6557e65f4560a08897c1a286a756
markov <markov@axsoft.ru>
2025-01-15 17:21:37 +0300

    fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { message } from 'telegraf/filters'
 const token = '7946503633:AAHvng-wwXe4jZ8smNs9NbPKaF-U0FoBFpM'
 // const webAppUrl = 'https://autotest.service.ag.mos.ru/mini-app/polls/11622/simple/?uid=8dokoC&channel=tg'
 const webAppUrl = 'https://ag-vmeste.ru/main'
-const gz = 'https://gz.mos.ru/u/main'
+const gz = 'https://gz.mos.ru'
 const bot = new Telegraf(token)
 bot.command('start', (ctx) => {
     ctx.reply('Привет, это тестовое сообщение',
