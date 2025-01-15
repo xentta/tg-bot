4b586832f3e2aa8d77b63f89ad3a86025890bf81
markov <markov@axsoft.ru>
2025-01-15 17:18:59 +0300

    add more button

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ import { Telegraf, Markup } from 'telegraf'
 import { message } from 'telegraf/filters'
 
 const token = '7946503633:AAHvng-wwXe4jZ8smNs9NbPKaF-U0FoBFpM'
-const webAppUrl = 'https://autotest.service.ag.mos.ru/mini-app/polls/11622/simple/?uid=8dokoC&channel=tg'
+// const webAppUrl = 'https://autotest.service.ag.mos.ru/mini-app/polls/11622/simple/?uid=8dokoC&channel=tg'
+const webAppUrl = 'https://ag-vmeste.ru/main'
+const gz = 'https://gz.mos.ru/u/main'
 const bot = new Telegraf(token)
 bot.command('start', (ctx) => {
     ctx.reply('Привет, это тестовое сообщение',
-    Markup.keyboard([Markup.button.webApp('Открыть приложение Город  Заданий', webAppUrl)]).resize())
+    Markup.keyboard([
+        [
+          Markup.button.webApp('Тестируем голосования', webAppUrl),
+          Markup.button.webApp('Город  Заданий', gz)
+        ]
+    ]).resize())
 })
 bot.on(message('web_app_data'), async ctx => {
     const data = ctx.webAppData.data.json()
